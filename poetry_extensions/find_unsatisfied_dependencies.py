9a0417ce28aa8101dc2296492db61456653bad61
likianta <likianta@foxmail.com>
2024-07-24 11:11:55 +0800

    self made deps tracker based on ast

--- poetry_extensions/find_unsatisfied_dependencies.py
+++ poetry_extensions/find_unsatisfied_dependencies.py
@@ -1,70 +1,74 @@
 """
-req: deptry
+use ast to parse the source code and find all imports.
+if imports in pyproject.toml are defined but not used across the source code, print them out.
 """
-import sys
-import typing as t
 
+import ast
+
+import lk_logger
 from argsense import cli
 from lk_utils import fs
-from lk_utils import load
-from lk_utils import run_cmd_args
+
+lk_logger.update(show_varnames=True)
 
 
 @cli.cmd()
-def main(source_dir: str) -> None:
+def main(source_dir: str, include_dev_group: bool = False) -> None:
+    """
+    required:
+        <user_project>
+        |- <source>         # <- `source_dir`
+        |- pyproject.toml
+    """
     project_dir = fs.parent(source_dir)
-    assert fs.exists(f'{project_dir}/pyproject.toml')
-    
-    run_cmd_args(
-        sys.executable, '-m', 'deptry', source_dir,
-        '--config', f'{project_dir}/pyproject.toml',
-        '--json-output', fs.xpath('../temp.json'),
-        # verbose=True,
-        ignore_error=True
-    )
-    
-    missing = set()
-    redundant = set()
-    # unknown = set()
-    
-    log: list = fs.load(fs.xpath('../temp.json'))
-    for xdict in log:
-        match xdict['error']['code']:
-            case 'DEP001':
-                missing.add(xdict['module'])
-            case 'DEP002':
-                redundant.add(xdict['module'])
-            case 'DEP004':
-                pass
-            case _:
-                print('unrecognized code', xdict, ':v3l')
-    
-    ignores = _load_custom_rules(f'{project_dir}/pyproject.toml')
-    for x in sorted(missing):
-        if x not in ignores:
-            print(f'missing: {x}', ':v4s1i2')
-    for x in sorted(redundant):
-        if x not in ignores:
-            print(f'redundant: {x}', ':v3s1i2')
+    assert fs.exists(f"{project_dir}/pyproject.toml")
 
+    imported_packages = set()  # {top_name, ...}
+    for f in fs.findall_files(source_dir, ".py"):
+        code = fs.load(f.path)
+        tree = ast.parse(code)
+        for node in ast.walk(tree):
+            if isinstance(node, ast.Import):
+                for alias in node.names:
+                    top_name = alias.name.split(".")[0]
+                    imported_packages.add(top_name)
+            elif isinstance(node, ast.ImportFrom):
+                if node.module:
+                    top_name = node.module.split(".")[0]
+                    imported_packages.add(top_name)
+    print(len(imported_packages))
 
-@cli.cmd()
-def show_help() -> None:
-    run_cmd_args(
-        sys.executable, '-m', 'deptry', '--help',
-        verbose=True,
-    )
+    defined_packages = set()
+    conf = fs.load(f"{project_dir}/pyproject.toml")
+    for x in conf["tool"]["poetry"]["dependencies"]:
+        if x == "python":
+            continue
+        name = x.replace("-", "_")
+        defined_packages.add(name)
+    if "group" in conf["tool"]["poetry"]:
+        for k, x in conf["tool"]["poetry"]["group"].items():
+            if not include_dev_group and k == "dev":
+                continue
+            for y in x["dependencies"]:
+                name = y.replace("-", "_")
+                defined_packages.add(name)
+    print(len(defined_packages))
 
-
-def _load_custom_rules(pyproj_file: str) -> t.Sequence[str]:
-    conf = load(pyproj_file)
     try:
-        return conf['tool']['poetry_extensions']['deptry']['ignores']
-    except KeyError:
-        return ()
+        ignored = frozenset(
+            x.replace("-", "_")
+            for x in conf["tool"]["poetry-extensions"]["ignored_redundant_packages"]
+        )
+    except Exception:
+        ignored = frozenset()
+
+    for x in sorted(defined_packages):
+        if x not in imported_packages:
+            if x not in ignored:
+                print(x, ":iv3")
 
 
-if __name__ == '__main__':
-    # pox poetry_extensions/find_unsatisfied_dependencies.py show-help
-    # pox poetry_extensions/find_unsatisfied_dependencies.py main <dir>
-    cli.run()
+if __name__ == "__main__":
+    # pox poetry_extensions/find_unsatisfied_dependencies.py <src_dir>
+    # pox poetry_extensions/find_unsatisfied_dependencies.py <src_dir> :true
+    cli.run(main)
